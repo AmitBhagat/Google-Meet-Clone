6374fecc4836065fa42228e355e01dc1ebb28015
Amit Bhagat <abbhagat27@gmail.com>
2022-03-21 14:25:11 +0530

    link copy

--- public/js/room.js
+++ public/js/room.js
@@ -168,26 +168,8 @@ document.querySelector('.roomcode').innerHTML = `${roomid}`
 
 function CopyClassText() {
 
-    var textToCopy = document.querySelector('.roomcode');
-    var currentRange;
-    if (document.getSelection().rangeCount > 0) {
-        currentRange = document.getSelection().getRangeAt(0);
-        window.getSelection().removeRange(currentRange);
-    }
-    else {
-        currentRange = false;
-    }
-
-    var CopyRange = document.createRange();
-    CopyRange.selectNode(textToCopy);
-    window.getSelection().addRange(CopyRange);
-    document.execCommand("copy");
-
-    window.getSelection().removeRange(CopyRange);
-
-    if (currentRange) {
-        window.getSelection().addRange(currentRange);
-    }
+    var textToCopy = window.location.href;
+    window.navigator.clipboard.writeText(textToCopy);
 
     document.querySelector(".copycode-button").textContent = "Copied!"
     setTimeout(()=>{
